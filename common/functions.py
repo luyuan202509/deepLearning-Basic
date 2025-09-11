6e8238d78897d10f7d413dd4764fe329af733c87
luyuan <luyuan202509@gmail.com>
2025-09-11 19:18:15 +0800

    恒等函数和softmax函数

--- common/functions.py
+++ common/functions.py
@@ -4,8 +4,17 @@ import numpy as np
 # 激活函数
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
-
 # 越迁函数
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
+
+''' 输出问题 '''
+# 恒等函数
+def identity_function (x):
+   return x 
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y 

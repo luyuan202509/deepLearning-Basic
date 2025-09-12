6e018cc252e5f3b4ac08c5acf498e18bc9098644
luyuan <luyuan202509@gmail.com>
2025-09-12 13:33:44 +0800

    交叉熵误差优化

--- common/functions.py
+++ common/functions.py
@@ -26,4 +26,5 @@ def mean_squared_error(y,t):
     return 0.5 * np.sum(y-t**2)
 # 交叉熵误差
 def cross_entropy_error(y,t):
-    return -np.sum(t * np.log(y))
+    delt  = 1e-7
+    return -np.sum(t * np.log(y-delt))

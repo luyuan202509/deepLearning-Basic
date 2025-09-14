f09fea84f396b87022928d9b8cfa0fa6b97d6fca
luyuan <luyuan202509@gmail.com>
2025-09-14 21:27:32 +0800

    Affine层，以矩阵为对象的反向传播过程（前面几次提交的反向传播都是在标量，一个数字结果反向传播）；这里以之前的y=X*W +B过程是用numpy.array矩阵的方式进行向前传播（predict）,对它进行反向传播

--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functions import sigmoid
 class Relu:
     def __init__(self):
         self.mask = None
@@ -17,3 +18,34 @@ class Relu:
         dout[self.mask] = 0
         dx = dout
         return dx 
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self,x):
+        out = sigmoid(x)
+        self.out = out
+        return out
+    def backword(self,dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+class Affine:
+    def __init__(self,W,b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dw = None
+        self.db = None
+        
+    def forward(self,x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+
+    def backword(self,dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx

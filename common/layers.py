f81cdf556c61ea8f144bd318db68000e3ed9bea1
luyuan <luyuan202509@gmail.com>
2025-09-15 11:52:42 +0800

    Softmax-with-Loss层的实现

--- common/layers.py
+++ common/layers.py
@@ -1,5 +1,5 @@
 import numpy as np
-from functions import sigmoid
+from functions import sigmoid,softmax,cross_entropy_error
 class Relu:
     def __init__(self):
         self.mask = None
@@ -48,4 +48,21 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
+        return dx
+
+class SoftmaxWithLoss():
+    '''Softmax-with-Loss层的实现'''
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+    
+    def forward(self,x,t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+    
+    def backward(self,dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t)/batch_size
         return dx

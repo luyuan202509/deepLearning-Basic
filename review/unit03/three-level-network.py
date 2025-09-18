a9c295380c0e0632e8ac56ea63ec8cbd389ab9ca
luyuan <luyuan202509@gmail.com>
2025-09-18 16:59:10 +0800

    3层神经网络封装成函数 1初始化参数 2向前传播

--- review/unit03/three-level-network.py
+++ review/unit03/three-level-network.py
@@ -16,6 +16,7 @@ W1 = np.array([[0.1, 0.3, 0.5],
 b1 = np.array([0.1, 0.2, 0.3])
 A1 = np.dot(X, W1) + b1
 Z1 = segmoid(A1) 
+print(f'z1 shape: {Z1.shape}')
 
 W2 =np.array([[0.1, 0.4], 
              [0.2, 0.5],
@@ -24,11 +25,17 @@ b2 = np.array([0.1, 0.2])
 A2 =np.dot(Z1,W2) + b2
 Z2 = segmoid(A2)
 
+print(f'z2 shape: {Z2.shape}')
+
+
 W3 = np.array([[0.1, 0.3],
                [0.2, 0.4]])
 b3 = np.array([0.1, 0.2])
 A3 = np.dot(Z2,W3) +b3
 Z3 = segmoid(A3)
+
+print(f'z3 shape: {Z3.shape}')
+
 
 y = identity_function(A3)
 print(y)

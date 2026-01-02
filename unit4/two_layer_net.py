a77bd7eeeb0d8c3da5c936a30554416df4aacebc
luyuan <luyuan202509@gmail.com>
2026-01-02 20:32:04 +0800

    2层神经网络

---
+++ unit4/two_layer_net.py
@@ -0,0 +1,72 @@
+import numpy as np
+import sys, os
+# 添加项目根目录到 Python 路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+
+from common.functions import *
+
+class TwoLayerNet:
+    def __init__(self,input_size,hidden_size,output_size,weight_init_std=0.01):
+        # 初始化权重
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size,hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size,output_size)
+        self.params['b2'] = np.zeros(output_size)
+    
+    def predict(self,x):
+        W1,W2 = self.params['W1'],self.params['W2']
+        b1,b2 = self.params['b1'],self.params['b2']
+        a1 = np.dot(x,W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1,W2) + b2
+        y = softmax(a2)
+        return y 
+    
+    def loss(self,x,t):
+        y = self.predict(x)
+        return cross_entropy_error(y,t)
+    
+    def accuracy(self,x,t):
+        y = self.predict(x)
+        y = np.argmax(y,axis=1)
+        t = np.argmax(t,axis=1)
+        accuracy = np.sum(y==t)/float(x.shape[0])
+        return accuracy
+    
+    def numerical_gradient(self,x,t):
+        loss_W = lambda W: self.loss(x,t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W,self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W,self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W,self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W,self.params['b2'])
+
+        return grads
+
+
+
+
+def print_param(net):
+   
+    for i in range(len(net.params)):
+        #print(f"参数{net.params[i]}形状：{net.params[i].shape}")
+       print(net.params['W1'].shape)
+       print(net.params['b1'].shape)
+       print(net.params['W2'].shape)
+       print(net.params['b2'].shape)
+def main():
+    net = TwoLayerNet(input_size=784,hidden_size=100,output_size=10)
+    print_param(net)
+    x = np.random.rand(100,784)
+    t = np.random.rand(100,10)
+    grads = net.numerical_gradient(x,t)
+
+
+if __name__ == '__main__':
+    main()

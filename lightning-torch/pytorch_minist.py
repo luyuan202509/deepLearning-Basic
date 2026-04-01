c4dae81a27d9c528e4959e61721db28dbee9b6d3
luyuan <luyuan202509@gmail.com>
2026-04-01 16:08:30 +0800

    torch版本训练过程

---
+++ lightning-torch/pytorch_minist.py
@@ -0,0 +1,37 @@
+import os,sys 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset,random_split
+
+from torchvision.datasets import MNIST
+from torchvision import transforms
+
+from simple_dense_net import SimpleDenseNet
+
+
+
+
+
+if __name__ == "__main__":
+    model = SimpleDenseNet()
+    optimizer = optim.SGD(model.parameters(), lr=0.01)  # 随机梯度下降优化器
+    criterion = nn.CrossEntropyLoss()  # 交叉熵损失函数,适用于多分类问题
+    
+    dataset = MNIST(root='/Users/luyuan/neulife/pyproject/deepLearning-Basic/data', 
+                    train=True, download=True, transform=transforms.ToTensor())
+    train_loader = DataLoader(dataset, batch_size=64, shuffle=True)
+
+    for epoch in range(10):
+        for data in train_loader:
+            inputs,labels = data
+            optimizer.zero_grad() # 清零梯度
+            output = model(inputs) # 前向传播
+            loss = criterion(output, labels) # 计算损失
+            loss.backward() # 反向传播
+            optimizer.step() # 更新参数
+            #if batch_idx % 100 == 0: # 每100个批次打印一次损失
+            #    print(f'Epoch {epoch}, Batch {batch_idx}, Loss: {loss.item()}')
+        print(f'Epoch [{epoch +1} / 10],loss: {loss.item():.4f}')
---
+++ lightning-torch/simple_dense_net.py
@@ -0,0 +1,32 @@
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+
+
+class SimpleDenseNet(nn.Module):
+    def __init__(self, 
+                input_size:int=784, 
+                lin1_size:int=256, 
+                lin2_size:int=256, 
+                lin3_size:int=256, 
+                output_size:int=10):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size, lin1_size),
+            nn.ReLU(),
+            nn.Linear(lin1_size, lin2_size),
+            nn.ReLU(),
+            nn.Linear(lin2_size, lin3_size),
+            nn.ReLU(),
+            nn.Linear(lin3_size, output_size)
+        )
+
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        batch_size,channels,width,height = x.size()
+        x = x.view(batch_size, -1)
+        return self.model(x)
+
+
+if __name__ == "__main__":
+    net = SimpleDenseNet()
+    print(net)
